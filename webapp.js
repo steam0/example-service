bcacffd16f8bd6363a2c295b77fad60dcf94a89f
Henrik Stene <henrik.stene@knowit.no>
2017-09-21 12:43:27 +0200

    Updated example

--- webapp.js
+++ webapp.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
+var requestClient = require('request');
 app.use(cors());
 
 var server = app.listen(3000, function() {
@@ -11,9 +12,48 @@ var server = app.listen(3000, function() {
 	console.log('Server is listening at http://%s:%s', host, port);
 });
 
-app.get('/test', function (request, response) {
-	console.log("Testing auth");
-	console.log(request.query);
+// Helpers
 
-	response.end();
+
+
+// API
+
+app.get('/test', function (request, response) {
+	//console.log(request);
+	//console.log(request.query);
+
+	authorize(request, response, function (err, res, body) {
+			console.log("Yes");
+			console.log(err);
+			console.log(body);
+			/*var body = {
+				"working": "yes",
+				"testing": "no"
+			}*/
+			response.set('Content-Type', 'application/json');
+			return response.send(body);
+	});
 });
+
+
+function authorize(request, response, callback) {
+	var options = {
+		url: 'http://localhost:8080/authorize',
+		method: 'GET',
+		headers: {
+			'Authorization': request.headers.authorization
+		}
+	};
+
+	requestClient(options, function(err, res, body) {
+		 if (res && (res.statusCode === 200 || res.statusCode === 201)) {
+			 callback(err, res, body);
+		 } else {
+			 console.log("Access denied");
+			 response.statusCode = 500
+			 response.body = body;
+			 return response.end();
+		 }
+	 });
+
+}
