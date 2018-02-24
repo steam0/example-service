277d66cee4bfc1ec13e074422ab6468e1e552de3
Modio Webserver <hstene@me.com>
2018-02-24 12:57:55 +0100

    Updated local ip in build script

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ SERVICE_NAME=example-service
 git pull;
 docker build -t $SERVICE_NAME -f x86/Dockerfile .
 docker rm -f $SERVICE_NAME;
-docker run -d -p 3003:3000 --name $SERVICE_NAME --restart always -e AUTH_SERVER_IP="10.0.1.7" -e AUTH_SERVER_PORT=8000 -e KAFKA_IP="10.0.1.7" -e KAFKA_PORT=2181 $SERVICE_NAME
+docker run -d -p 3003:3000 --name $SERVICE_NAME --restart always -e AUTH_SERVER_IP="192.168.86.2" -e AUTH_SERVER_PORT=8000 -e KAFKA_IP="192.168.86.2" -e KAFKA_PORT=2181 $SERVICE_NAME
